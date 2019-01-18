7d4f160b81a7e55767b2b119a289598a6161fe4e
hritupon <hritupon.saikia@gmail.com>
2019-01-19 02:56:29 +0530

    003:adding menu items to bottom navigation

--- Android/app/src/main/java/com/staytouch/MainActivity.java
+++ Android/app/src/main/java/com/staytouch/MainActivity.java
@@ -2,12 +2,25 @@ package com.staytouch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.staytouch.Utils.BottomNavigationViewHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting");
+        setupBottomNavigationView();
+    }
+
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up bottom navigation view");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
---
+++ Android/app/src/main/java/com/staytouch/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,16 @@
+package com.staytouch.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+    
+    public static void setupBottomNavigationView (BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+    }
+}
