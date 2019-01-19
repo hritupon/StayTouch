e44aaf61d12ed22f0f731616e426b392bbe34900
hritupon <hritupon.saikia@gmail.com>
2019-01-20 00:01:33 +0530

    011: Account settings navigation setup done.

--- Android/app/src/main/java/com/staytouch/Profile/AccountSettingsActivity.java
+++ Android/app/src/main/java/com/staytouch/Profile/AccountSettingsActivity.java
@@ -1,18 +1,50 @@
 package com.staytouch.Profile;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.staytouch.R;
 
+import java.util.ArrayList;
+
 public class AccountSettingsActivity extends AppCompatActivity {
     private static final String TAG = "AccountSettingsActivity";
+    private Context mContext;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+        mContext = AccountSettingsActivity.this;
         Log.d(TAG, "onCreate: started");
+        setupSettingsList();
+
+        //setup the back arrow action
+
+        ImageView backArrow =(ImageView) findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating back to ProfileActivity");
+                finish();
+            }
+        });
+    }
+
+    private void setupSettingsList(){
+        Log.d(TAG, "setupSettingsList: Initializing account settings list.");
+        ListView listView = (ListView)findViewById(R.id.listViewAccountSettings);
+        ArrayList<String> options = new ArrayList<>();
+        options.add(getString(R.string.edit_profile));
+        options.add(getString(R.string.sign_out));
+
+        ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
     }
 }
--- Android/app/src/main/java/com/staytouch/Profile/ProfileActivity.java
+++ Android/app/src/main/java/com/staytouch/Profile/ProfileActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.staytouch.R;
@@ -20,6 +21,8 @@ public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity";
 
     private Context mContext = ProfileActivity.this;
+    private ProgressBar mProgressBar;
+
     private static final int ACTIVITY_NUM = 4;
 
     @Override
@@ -27,6 +30,8 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
+        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        mProgressBar.setVisibility(View.GONE);
         setupBottomNavigationView();
         setupToolbar();
     }
