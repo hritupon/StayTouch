fd9faf9364f936c5e8194f3fe7196390770d9a9f
hritupon <hritupon.saikia@gmail.com>
2019-01-30 16:20:05 +0530

    014: Edit Profile - Universal Image Loader Config

--- Android/app/src/main/java/com/staytouch/Profile/EditProfileFragment.java
+++ Android/app/src/main/java/com/staytouch/Profile/EditProfileFragment.java
@@ -4,18 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.staytouch.R;
+import com.staytouch.Utils.UniversalImageLoader;
 
 public class EditProfileFragment extends Fragment {
+
     private static final String TAG = "EditProfileFragment";
+    private ImageView mProfilePhoto;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+        initImageLoader();
+        setProfileImage();
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imageUrl = "images6.fanpop.com/image/photos/36600000/Sherlock-Holmes-Sherlock-BBC1-image-sherlock-holmes-sherlock-bbc1-36692648-267-400.jpg";
+        UniversalImageLoader.setImage(imageUrl, mProfilePhoto, null, "http://");
     }
 }
--- Android/app/src/main/java/com/staytouch/Utils/BottomNavigationViewHelper.java
+++ Android/app/src/main/java/com/staytouch/Utils/BottomNavigationViewHelper.java
@@ -31,23 +31,23 @@ public class BottomNavigationViewHelper {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.ic_house:
-                        Intent intent1 = new Intent(context, MainActivity.class);//ACTIVITY_NUM = 0
+                        Intent intent1 = new Intent(context, MainActivity.class);//ACTIVITY_NUM=0
                         context.startActivity(intent1);
                         break;
                     case R.id.ic_search:
-                        Intent intent2 = new Intent(context, SearchActivity.class);//ACTIVITY_NUM = 1
+                        Intent intent2 = new Intent(context, SearchActivity.class);//ACTIVITY_NUM=1
                         context.startActivity(intent2);
                         break;
                     case R.id.ic_circle:
-                        Intent intent3 = new Intent(context, ShareActivity.class);//ACTIVITY_NUM = 2
+                        Intent intent3 = new Intent(context, ShareActivity.class);//ACTIVITY_NUM=2
                         context.startActivity(intent3);
                         break;
                     case R.id.ic_alert:
-                        Intent intent4 = new Intent(context, LikesActivity.class);//ACTIVITY_NUM = 3
+                        Intent intent4 = new Intent(context, LikesActivity.class);//ACTIVITY_NUM=3
                         context.startActivity(intent4);
                         break;
                     case R.id.ic_profile:
-                        Intent intent5 = new Intent(context, ProfileActivity.class);//ACTIVITY_NUM = 4
+                        Intent intent5 = new Intent(context, ProfileActivity.class);//ACTIVITY_NUM=4
                         context.startActivity(intent5);
                         break;
                 }
