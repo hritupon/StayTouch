1fda98a2ae505e7726aa8ad45ea5a9d2e30c8446
hritupon <hritupon.saikia@gmail.com>
2019-02-09 15:58:50 +0530

    017: Login Layout

--- Android/app/src/main/java/com/staytouch/Likes/LikesActivity.java
+++ Android/app/src/main/java/com/staytouch/Likes/LikesActivity.java
@@ -21,9 +21,9 @@ public class LikesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Log.d(TAG, "onCreate: started.");
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
     }
 
     private void setupBottomNavigationView(){
